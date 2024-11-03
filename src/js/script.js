574c1c0c94f53131c13f4c87a79797bca77bc521
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-11-03 19:35:22 +0100

    Add locally-hosted fonts & add browser support for the favicon & add funcionality to convert between currencies

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ let leftSelect;
 let swapButton;
 let rightSelect;
 let convertButton;
+let errorMessage;
 let result;
 
 const URL = "https://api.frankfurter.app/currencies";
@@ -19,11 +20,12 @@ function getElements() {
   swapButton = document.querySelector(".app__body-swap-button");
   rightSelect = document.querySelector(".app__body-select--to");
   convertButton = document.querySelector(".app__body-convert-button");
+  errorMessage = document.querySelector(".app__body-error");
   result = document.querySelector(".app__body-result");
 };
 
 function addListeners() {
-  
+  convertButton.addEventListener("click", getCurrencies);
 };
 
 function renderLists() {
@@ -31,9 +33,36 @@ function renderLists() {
     .then((response) => response.json())
     .then((dataReady) => {
       Object.entries(dataReady).forEach((currency) => {
-        leftSelect.innerHTML += `<option value="${currency[0]}">${currency[1]}</option>`;
-        rightSelect.innerHTML += `<option value="${currency[0]}">${currency[1]}</option>`;
+        leftSelect.innerHTML += `<option value="${currency[0]}">${currency[1]} (${currency[0]})</option>`;
+        rightSelect.innerHTML += `<option value="${currency[0]}">${currency[1]} (${currency[0]})</option>`;
       });
+    })
+    .catch((error) => console.error(error));
+}
+
+const getCurrencies = () => {
+  let amount = amountInput.value;
+  let fromCurrency = leftSelect.value;
+  let toCurrency = rightSelect.value;
+
+  if (fromCurrency === toCurrency) {
+    errorMessage.setAttribute("aria-hidden", "false");
+    errorMessage.textContent = "Currencies should be different.";
+  } else {
+    errorMessage.setAttribute("aria-hidden", "true");
+    errorMessage.textContent = "";
+    convert(amount, fromCurrency, toCurrency);
+  }
+}
+
+const convert = (amount, fromCurrency, toCurrency) => {
+  fetch(`https://api.frankfurter.app/latest?amount=${Number(amount)}&from=${fromCurrency}&to=${toCurrency}`)
+    .then((response) => response.json())
+    .then((dataReady) => {
+      const data = Object.entries(dataReady.rates)[0];
+
+      result.setAttribute("aria-hidden", "false");
+      result.innerHTML = `${dataReady.amount + " " + dataReady.base} = ${data[1] + " " + data[0]}`;
     })
     .catch((error) => console.error(error));
 }
